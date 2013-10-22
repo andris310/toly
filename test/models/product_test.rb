8032e76d1e6085c1b73ca68099ec736a57992fcf
andris310 <andris310@gmail.com>
2013-10-21 22:24:22 -0700

    aded fixtures

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-
+  fixtures :products
   test 'product attributes must not be empty' do
     product = Product.new
     assert product.invalid?
@@ -17,12 +17,12 @@ class ProductTest < ActiveSupport::TestCase
                           image_url: 'some.jpg')
       product.price = -1
       assert product.invalid?
-      assert_equal ['must be greater then or equal to 0.01'],
+      assert_equal ['must be greater than or equal to 0.01'],
         product.errors[:price]
 
       product.price = 0
       assert product.invalid?
-      assert_equal ['must be greater then or equal to 0.01'],
+      assert_equal ['must be greater than or equal to 0.01'],
         product.errors[:price]
 
       product.price = 1
@@ -45,6 +45,16 @@ class ProductTest < ActiveSupport::TestCase
     bad.each do |name|
       assert new_product(name).invalid?, "#{name} shouldn't be valid"
     end
+  end
+
+  test "product is not valid without a unique title - i18n" do
+    product = Product.new(title: products(:ruby).title,
+                          description: 'yyy',
+                          price: 1,
+                          image_url: 'fred.gif')
+    assert product.invalid?
+    assert_equal [I18n.translate('errors.messages.taken')],
+                  product.errors[:title]
   end
 
 end
