a321df8ced3ef33ce3089dc3eed552869e45cd2c
andris310 <andris310@gmail.com>
2013-10-28 21:55:39 -0700

    order form validation

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,5 @@
 class Order < ActiveRecord::Base
-  PAYMENT_TYPES = ["Credit card", "Check"]
+  PAYMENT_TYPES = ["Credit card"]
+  validates :name, :address, :email, presence: true
+  validates :pay_type, inclusion: PAYMENT_TYPES
 end
