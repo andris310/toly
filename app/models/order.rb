3f199ff3415cc7cc2b7287e39b8df82fd45af9da
andris310 <andris310@gmail.com>
2013-11-06 22:01:50 -0800

    add states

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,19 @@
 class Order < ActiveRecord::Base
   has_many :line_items, dependent: :destroy
   PAYMENT_TYPES = ["Credit card"]
+  STATES = [
+      'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT',
+      'DE', 'DC', 'FL', 'GA', 'HI', 'ID', 'IL',
+      'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD',
+      'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE',
+      'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND',
+      'OH', 'OK', 'OR', 'PA', 'PR', 'RI', 'SC',
+      'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA',
+      'WV', 'WI', 'WY'
+    ]
   validates :first_name, :last_name, :address, :city, :state, :zipcode, :email, presence: true
   validates :pay_type, inclusion: PAYMENT_TYPES
+  validates :state, inclusion: STATES
 
   def add_line_items_from_cart(cart)
     cart.line_items.each do |item|
@@ -10,4 +21,5 @@ class Order < ActiveRecord::Base
       line_items << item
     end
   end
+
 end
