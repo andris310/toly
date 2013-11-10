13525cd5a7cb1b9868af850a59ce5ceecd9bb70f
andris310 <andris310@gmail.com>
2013-11-09 22:04:54 -0800

    secure users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,20 @@
 class UsersController < ApplicationController
+  before_filter :authenticate_owner!
 
   def show
     @user = User.find(params[:id])
+  end
+
+  private
+
+  def authenticate_owner!
+    if user_signed_in? && current_user.id == params[:id]
+      return
+    else
+      redirect_to '/',
+        :notice => "You must have permission to access this category."
+      return false
+    end
   end
 
 end
