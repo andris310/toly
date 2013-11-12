64ecee15458cf4cb279f083883bb6946c7526ecc
andris310 <andris310@gmail.com>
2013-11-11 21:25:13 -0800

    order total migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131110025027) do
+ActiveRecord::Schema.define(version: 20131112051230) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20131110025027) do
     t.string   "zipcode"
     t.string   "state"
     t.integer  "user_id"
+    t.decimal  "total_price"
   end
 
   create_table "products", force: true do |t|
