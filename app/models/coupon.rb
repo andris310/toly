04c653bdb68597885ccdbd25aed49989a5114e91
andris310 <andris310@gmail.com>
2013-11-12 21:22:53 -0800

    m

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,5 +1,5 @@
 class Coupon < ActiveRecord::Base
-  belongs_to :order
+
   COUPON_TYPES = ['Free Download', 'Dollar amount OFF ENTIRE ORDER',
                   'Dollar amount off PRODUCT', 'Percent off PRODUCT',
                   'Percent off ENTIRE ORDER']
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 class Order < ActiveRecord::Base
   has_many :line_items, dependent: :destroy
   has_many :products, through: :line_items
-  has_one :coupon
+
   belongs_to :user
   PAYMENT_TYPES = ["Credit card"]
   STATES = [
