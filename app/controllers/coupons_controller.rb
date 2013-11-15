2263990a08fb42bdccb46b91c1fa8c674fe81f8c
andris310 <andris310@gmail.com>
2013-11-15 14:45:46 -0800

    test

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -70,6 +70,6 @@ class CouponsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def coupon_params
-      params.require(:coupon).permit(:coupon_code, :name, :coupon_type, :usage, :discount_value)
+      params.require(:coupon).permit(:coupon_code, :times_used, :name, :coupon_type, :usage, :discount_value)
     end
 end
