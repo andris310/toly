11c4ccd64c97cc2e4add2c3d840e77caf9cabb6e
andris310 <andris310@gmail.com>
2013-11-16 20:45:08 -0800

    add save with payment

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,8 @@ class Order < ActiveRecord::Base
   has_many :line_items, dependent: :destroy
   has_many :products, through: :line_items
 
+  attr_accessor :stripe_card_token
+
   belongs_to :user
   PAYMENT_TYPES = ["Credit card"]
   STATES = [
@@ -39,10 +41,19 @@ class Order < ActiveRecord::Base
 
   def save_with_payment
     if valid?
-      charge = Stripe::Charge.create(description: email, amount: '400', card: stripe_card_token)
-      # self.stripe_customer_token = customer.id
+      charge = Stripe::Charge.create(
+        :description => email,
+        :amount => '400',
+        :currency => "usd",
+        :card => stripe_card_token)
+
+      self.stripe_customer_token = charge.id
       save!
     end
+    rescue Stripe::InvalidRequestError => e
+      logger.error "Stripe error while creating customer: #{e.message}"
+      errors.add :base, "There was a problem with your credit card."
+      false
   end
 
 end
