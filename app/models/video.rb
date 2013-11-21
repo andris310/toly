17fa96b0c9372ee7dc0983207fe6322ec8972f33
andris310 <andris310@gmail.com>
2013-11-20 18:45:47 -0800

    genereate video uploader

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,3 +1,8 @@
 class Video < ActiveRecord::Base
   has_and_belongs_to_many :products
+  validates :title, :description, presence: true
+  validates :title, uniqueness: true
+
+  mount_uploader :video_url, VideoUploader
+
 end
