2da591ce0504557de06772b2b18add4932728ea2
andris310 <andris310@gmail.com>
2013-11-24 15:34:39 -0800

    add discount column to orders

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131122221222) do
+ActiveRecord::Schema.define(version: 20131124232240) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 20131122221222) do
     t.integer  "user_id"
     t.decimal  "total_price"
     t.string   "stripe_customer_token"
+    t.decimal  "discount",              precision: 8, scale: 2
   end
 
   create_table "products", force: true do |t|
