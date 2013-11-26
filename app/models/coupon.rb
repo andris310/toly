bedf00b1c206e674ad091e9d95aeacac7d6bedcf
andris310 <andris310@gmail.com>
2013-11-25 20:18:04 -0800

    add coupon column to orders

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,6 +1,7 @@
 class Coupon < ActiveRecord::Base
   belongs_to :product
   belongs_to :cart
+  belongs_to :order
   validates :name, :coupon_code, :coupon_type, presence: true, uniqueness: true
   validates :coupon_code, length: { is: 6 }
   COUPON_TYPES = ['Free PRODUCT']
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131124232240) do
+ActiveRecord::Schema.define(version: 20131126041313) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 20131124232240) do
     t.decimal  "total_price"
     t.string   "stripe_customer_token"
     t.decimal  "discount",              precision: 8, scale: 2
+    t.string   "coupon"
   end
 
   create_table "products", force: true do |t|
