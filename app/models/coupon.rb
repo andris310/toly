7dc3ecb044f4b494dda64c8bb67e2d24a82591d6
andris310 <andris310@gmail.com>
2013-11-30 17:39:02 -0800

    add percentage coupon

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -4,7 +4,7 @@ class Coupon < ActiveRecord::Base
   belongs_to :order
   validates :name, :coupon_code, :coupon_type, presence: true, uniqueness: true
   validates :coupon_code, length: { is: 6 }
-  COUPON_TYPES = ['Free PRODUCT']
+  COUPON_TYPES = ['Free PRODUCT', 'Order PERCENTAGE']
 end
 
 # 'Dollar amount OFF ENTIRE ORDER', 'Dollar amount off PRODUCT', 'Percent off PRODUCT', 'Percent off ENTIRE ORDER'
