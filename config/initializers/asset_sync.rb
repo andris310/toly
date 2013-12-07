d61d45066c23e9f2aac9190719a2d6341f681389
andris310 <andris310@gmail.com>
2013-12-07 11:23:37 -0800

    m

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -4,7 +4,7 @@ AssetSync.configure do |config|
   config.aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
   # To use AWS reduced redundancy storage.
   # config.aws_reduced_redundancy = true
-  config.fog_directory = ENV['ASSET_BUCKET']
+  config.fog_directory = ENV['FOG_DIRECTORY']
 
   # Invalidate a file on a cdn after uploading files
   # config.cdn_distribution_id = "12345"
