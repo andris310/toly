f1f51a6efebeb59abe1d69b11156be5b10e0eb04
andris310 <andris310@gmail.com>
2013-12-08 16:57:23 -0800

    change videos description column from string to text

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131207015222) do
+ActiveRecord::Schema.define(version: 20131209005503) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
@@ -117,7 +117,7 @@ ActiveRecord::Schema.define(version: 20131207015222) do
     t.string   "video_url"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "description"
+    t.text     "description", limit: 255
   end
 
 end
