4af906921079760fa64ac883644ec44dabf4b4eb
andris310 <andris310@gmail.com>
2013-12-10 21:34:28 -0800

    remove back button

--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -4,15 +4,12 @@ class VideoUploader < CarrierWave::Uploader::Base
   self.fog_public = false
   self.fog_authenticated_url_expiration = 120
 
-  # storage :file
   storage :fog
 
   def fog_directory
     ENV['AWS_VIDEO_BUCKET']
   end
 
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
