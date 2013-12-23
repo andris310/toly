83c632e155ac81554b5144e3a2e28def210e310f
andris310 <andris310@gmail.com>
2013-12-22 17:40:54 -0800

    menu helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,14 @@
 module ApplicationHelper
+
   def hidden_div_if(condition, attributes = {}, &block)
     if condition
       attributes['style'] = 'display: none'
     end
     content_tag('div', attributes, &block)
   end
+
+  def is_active?(page_name)
+    "active" if params[:action] == page_name
+  end
+
 end
