85237e34aa21754c6e5b4c631db6845e0932cf37
andris310 <andris310@gmail.com>
2013-12-29 17:01:10 -0800

    reload page when empty cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -77,7 +77,7 @@ class CartsController < ApplicationController
     @cart.destroy if @cart.id == session[:cart_id]
     session[:cart_id] = nil
     respond_to do |format|
-      format.js { }
+      format.js { render inline: "location.reload();" }
       format.html { redirect_to store_url }
       format.json { head :no_content }
     end
