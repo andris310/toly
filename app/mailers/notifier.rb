63315e32f41bee3852125838d8f33b8ca948ccbd
andris310 <andris310@gmail.com>
2014-01-10 20:34:02 -0700

    css

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -2,6 +2,6 @@ class Notifier < ActionMailer::Base
 # existing lines here...
   def error_occured(error)
     @error = error
-    mail :to => "andris310@gmail.com", :subject => 'TOLY App Error Incident'  ## replace your email id to receive mails
+    mail :to => ENV['TEST_EMAIL'], :subject => 'TOLY App Error Incident'  ## replace your email id to receive mails
   end
 end
