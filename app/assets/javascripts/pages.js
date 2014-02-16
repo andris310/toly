e6c5f6db8ebe3fd764f5952ae435543577090eb1
andris310 <andris310@gmail.com>
2014-02-15 20:23:04 -0800

    adjust link for contact

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  teacherQtip();
+  // teacherQtip();
   $('.bxslider').bxSlider({
     mode: 'fade',
     controls: false,
@@ -9,25 +9,25 @@ $(document).ready(function(){
 });
 
 
-function teacherQtip() {
-  $('.contact-info').each(function() {
-    var email = $(this).parent().data('email');
-    $(this).qtip({
-      content: '<a href="mailto:"' + email + '"target="_top" class="qtip_email">' + email + '</span></a></br><span>' + $(this).parent().data('phone') + '</span>',
-      show: 'mouseover',
-      hide: {
-        fixed: true,
-        delay: 200
-      },
-      style: {
-        classes: 'qtip-bootstrap'
-      },
-      position: {
-         my: 'center left',
-         at: 'center right'
-      }
-    });
-  });
-}
+// function teacherQtip() {
+//   $('.contact-info').each(function() {
+//     var email = $(this).parent().data('email');
+//     $(this).qtip({
+//       content: '<a href="mailto:"' + email + '"target="_top" class="qtip_email">' + email + '</span></a></br><span>' + $(this).parent().data('phone') + '</span>',
+//       show: 'mouseover',
+//       hide: {
+//         fixed: true,
+//         delay: 200
+//       },
+//       style: {
+//         classes: 'qtip-bootstrap'
+//       },
+//       position: {
+//          my: 'center left',
+//          at: 'center right'
+//       }
+//     });
+//   });
+// }
 
-$(document).on('page:load', teacherQtip);
+// $(document).on('page:load', teacherQtip);
