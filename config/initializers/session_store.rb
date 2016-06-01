bf9c0e78e3c16558e0b9d7f816bef4a5d4bfbd34
andris310 <andris310@gmail.com>
2016-05-31 19:35:29 -0700

    fix password reset page

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Toly2Store::Application.config.session_store :cookie_store, key: '_toly2_store_session'
+Toly2Store::Application.config.session_store :cookie_store, key: '_toly2_store_session', expire_after: 24.hours
