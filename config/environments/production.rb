769dc2726e36782727f9f37be1ba0732aed75464
andris310 <andris310@gmail.com>
2016-05-31 20:32:59 -0700

    fix prod config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,13 +70,14 @@ Toly2Store::Application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
     :port    => 587,
-    :domain  => "treeoflifespinetherapy.com",
+    :domain  => "smtp.gmail.com",
     :user_name => ENV['GMAIL_USERNAME'],
     :password => ENV['GMAIL_PASSWORD'],
-    :authentication => 'plain',
+    :authentication => :plain,
     :enable_starttls_auto => true,
   }
 
