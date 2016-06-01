8c6de2fe6be2c60f3c257508b1561b9353f12b7f
andris310 <andris310@gmail.com>
2016-05-31 19:53:25 -0700

    update action_mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,13 +19,15 @@ Toly2Store::Application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
     :port    => 587,
-    :domain  => "mysite.com",
+    :domain  => "treeoflifespinetherapy.com",
     :user_name => ENV['GMAIL_USERNAME'],
     :password => ENV['GMAIL_PASSWORD'],
-    :authentication => 'plain',
+    :authentication => :plain,
     :enable_starttls_auto => true,
   }
 
--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -70,15 +70,16 @@ Toly2Store::Application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
-      :address => "smtp.gmail.com",
-      :port    => 587,
-      :domain  => "mysite.com",
-      :user_name => ENV['GMAIL_USERNAME'],
-      :password => ENV['GMAIL_PASSWORD'],
-      :authentication => 'plain',
-      :enable_starttls_auto => true,
-    }
+    :address => "smtp.gmail.com",
+    :port    => 587,
+    :domain  => "smtp.gmail.com",
+    :user_name => ENV['GMAIL_USERNAME'],
+    :password => ENV['GMAIL_PASSWORD'],
+    :authentication => :plain,
+    :enable_starttls_auto => true,
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
